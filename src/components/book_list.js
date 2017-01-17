0440d83e95afa1e7973058d6c2c26dabaebb6ef8
Riza Fahmi <rizafahmi@gmail.com>
2017-01-17 17:25:28 +0700

    Clean up and add Redux storage

---
+++ src/components/book_list.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class BookList extends React.Component {
+  renderList () {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className='list-group-item'>{book.title}</li>
+      )
+    })
+  }
+  render () {
+    return (
+      <ul className='list-group'>
+        {this.renderList()}
+      </ul>
+    )
+
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import App from './App'
+
+import reducers from './reducers'
+
+const createStoreMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreMiddleware(reducers)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
