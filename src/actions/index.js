3e11b4e8846d833f76373b80d4eac7a3fd02fd21
Riza Fahmi <rizafahmi@gmail.com>
2017-01-18 13:54:08 +0700

    Setting up action creator (dispatching)

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export const selectBook = (book) => {
+  console.log('A book has been selected: ', book.title)
+}
--- src/components/book_list.js
+++ src/components/book_list.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+// import components
+import { selectBook } from '../actions/index.js'
 
 class BookList extends React.Component {
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>{book.title}</li>
+        <li 
+          key={book.title} 
+          onClick={() => this.props.selectBook(book)}
+        className='list-group-item'>{book.title}</li>
       )
     })
   }
@@ -20,9 +27,17 @@ class BookList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  // Whatever is returned will show up as props inside of BookList
   return {
     books: state.books
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+// Make selectBook available through Props
+const mapDispatchToProps = (dispatch) => {
+  // Whenever selectBook is called, the result should be passed
+  // to all our reducers
+  return bindActionCreators({selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
